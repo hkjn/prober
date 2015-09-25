0e53a4173f94111ff7393bf625d796a1880f82fa
Henrik Jonsson <me@hkjn.me>
2015-09-25 11:41:05 +0200

    Update all godep dependencies to latest versions

--- Godeps/_workspace/src/hkjn.me/timeutils/time_test.go
+++ Godeps/_workspace/src/hkjn.me/timeutils/time_test.go
@@ -6,19 +6,43 @@ import (
 )
 
 func TestWeekday(t *testing.T) {
-	cases := map[time.Weekday]int{
-		time.Monday:    0,
-		time.Tuesday:   1,
-		time.Wednesday: 2,
-		time.Thursday:  3,
-		time.Friday:    4,
-		time.Saturday:  5,
-		time.Sunday:    6,
+	cases := []struct {
+		in   time.Weekday
+		want int
+	}{
+		{
+			in:   time.Monday,
+			want: 0,
+		},
+		{
+			in:   time.Tuesday,
+			want: 1,
+		},
+		{
+			in:   time.Wednesday,
+			want: 2,
+		},
+		{
+			in:   time.Thursday,
+			want: 3,
+		},
+		{
+			in:   time.Friday,
+			want: 4,
+		},
+		{
+			in:   time.Saturday,
+			want: 5,
+		},
+		{
+			in:   time.Sunday,
+			want: 6,
+		},
 	}
-	for in, exp := range cases {
-		out := Weekday(in)
-		if exp != out {
-			t.Fatalf("Weekday(%s) => %d; want %d\n", in, out, exp)
+	for i, tt := range cases {
+		out := Weekday(tt.in)
+		if tt.want != out {
+			t.Errorf("[%d] Weekday(%s) => %d; want %d\n", i, tt.in, out, tt.want)
 		}
 	}
 }
@@ -45,10 +69,10 @@ func TestAsMillis(t *testing.T) {
 			want:   1375286340000,
 		},
 	}
-	for _, tt := range cases {
+	for i, tt := range cases {
 		got := AsMillis(tt.t, tt.offset)
 		if got != tt.want {
-			t.Fatalf("AsMillis(%v, %d) => %d; want %d\n", tt.t, tt.offset, got, tt.want)
+			t.Errorf("[%d] AsMillis(%v, %d) => %d; want %d\n", i, tt.t, tt.offset, got, tt.want)
 		}
 	}
 }
@@ -64,55 +88,121 @@ func TestParse(t *testing.T) {
 		{"2013", "06", "31", "23:59", time.UTC, Must(ParseStd("2013-07-31 23:59"))},
 		{"2013", "05", "30", "23:59", time.UTC, Must(ParseStd("2013-06-30 23:59"))},
 	}
-	for _, tt := range cases {
+	for i, tt := range cases {
 		got, err := Parse(tt.year, tt.month, tt.day, tt.hourMinute, tt.loc)
 		if err != nil {
-			t.Fatalf("Parse(%q, %q, %q, %q, %v) got err %v\n", tt.year, tt.month, tt.day, tt.hourMinute, tt.loc, err)
+			t.Errorf("[%d] Parse(%q, %q, %q, %q, %v) got err %v\n", i, tt.year, tt.month, tt.day, tt.hourMinute, tt.loc, err)
 		}
 		if got != tt.want {
-			t.Fatalf("Parse(%q, %q, %q, %q, %v) => %v; want %v\n", tt.year, tt.month, tt.day, tt.hourMinute, got, tt.want)
+			t.Errorf("[%d] Parse(%q, %q, %q, %q, %v) => %v; want %v\n", i, tt.year, tt.month, tt.day, tt.hourMinute, tt.loc, got, tt.want)
 		}
 	}
 }
 
 func TestStartOfWeek(t *testing.T) {
-	cases := map[time.Time]time.Time{
-		Must(time.Parse("2006-01-02 15:04:05.000", "2014-07-07 00:00:00.000")): Must(ParseStd("2014-07-07 00:00")),
-		Must(ParseStd("2014-07-07 00:00")):                                     Must(ParseStd("2014-07-07 00:00")),
-		Must(ParseStd("2014-07-07 00:01")):                                     Must(ParseStd("2014-07-07 00:00")),
-		Must(ParseStd("2014-07-07 23:59")):                                     Must(ParseStd("2014-07-07 00:00")),
-		Must(ParseStd("2014-07-13 23:59")):                                     Must(ParseStd("2014-07-07 00:00")),
-		Must(time.Parse("2006-01-02 15:04:05.000", "2014-07-13 23:59:59.999")): Must(ParseStd("2014-07-07 00:00")),
-		Must(time.Parse("2006-01-02 15:04:05.000", "2014-07-14 00:00:00.000")): Must(ParseStd("2014-07-14 00:00")),
+	cases := []struct {
+		in   time.Time
+		want time.Time
+	}{
+		{
+			in:   Must(time.Parse("2006-01-02 15:04:05.000", "2014-07-07 00:00:00.000")),
+			want: Must(ParseStd("2014-07-07 00:00")),
+		},
+		{
+			in:   Must(ParseStd("2014-07-07 00:00")),
+			want: Must(ParseStd("2014-07-07 00:00")),
+		},
+		{
+			in:   Must(ParseStd("2014-07-07 00:01")),
+			want: Must(ParseStd("2014-07-07 00:00")),
+		},
+		{
+			in:   Must(ParseStd("2014-07-07 23:59")),
+			want: Must(ParseStd("2014-07-07 00:00")),
+		},
+		{
+			in:   Must(ParseStd("2014-07-13 23:59")),
+			want: Must(ParseStd("2014-07-07 00:00")),
+		},
+		{
+			in:   Must(time.Parse("2006-01-02 15:04:05.000", "2014-07-13 23:59:59.999")),
+			want: Must(ParseStd("2014-07-07 00:00")),
+		},
+		{
+			in:   Must(time.Parse("2006-01-02 15:04:05.000", "2014-07-14 00:00:00.000")),
+			want: Must(ParseStd("2014-07-14 00:00")),
+		},
 	}
-	for in, exp := range cases {
-		out := StartOfWeek(in)
-		if exp != out {
-			t.Fatalf("StartOfWeek(%v) => %v; want %v\n", in, out, exp)
+	for i, tt := range cases {
+		out := StartOfWeek(tt.in)
+		if tt.want != out {
+			t.Errorf("[%d] StartOfWeek(%v) => %v; want %v\n", i, tt.in, out, tt.want)
 		}
 	}
 }
 
 func TestDescDuration(t *testing.T) {
-	cases := map[time.Duration]string{
-		time.Millisecond:                 "0.0 sec ago",
-		time.Millisecond * 49:            "0.0 sec ago",
-		time.Millisecond * 50:            "0.1 sec ago",
-		time.Second:                      "1.0 sec ago",
-		time.Millisecond * 500:           "0.5 sec ago",
-		time.Second * 59:                 "59.0 sec ago",
-		time.Second*60 - 1:               "60.0 sec ago",
-		time.Second*60 + 1:               "1.0 min ago",
-		time.Minute:                      "1.0 min ago",
-		time.Minute * 60:                 "1.0 hrs ago",
-		time.Hour*24 - 1:                 "24.0 hrs ago",
-		time.Hour * 24:                   "1.0 days ago",
-		time.Hour*24*10e4 + time.Hour*12: "100000.5 days ago",
+	cases := []struct {
+		in   time.Duration
+		want string
+	}{
+		{
+			in:   time.Millisecond,
+			want: "0.0 sec ago",
+		},
+		{
+			in:   time.Millisecond * 49,
+			want: "0.0 sec ago",
+		},
+		{
+			in:   time.Millisecond * 50,
+			want: "0.1 sec ago",
+		},
+		{
+			in:   time.Second,
+			want: "1.0 sec ago",
+		},
+		{
+			in:   time.Millisecond * 500,
+			want: "0.5 sec ago",
+		},
+		{
+			in:   time.Second * 59,
+			want: "59.0 sec ago",
+		},
+		{
+			in:   time.Second*60 - 1,
+			want: "60.0 sec ago",
+		},
+		{
+			in:   time.Second*60 + 1,
+			want: "1.0 min ago",
+		},
+		{
+			in:   time.Minute,
+			want: "1.0 min ago",
+		},
+		{
+			in:   time.Minute * 60,
+			want: "1.0 hrs ago",
+		},
+		{
+			in:   time.Hour*24 - 1,
+			want: "24.0 hrs ago",
+		},
+		{
+			in:   time.Hour * 24,
+			want: "1.0 days ago",
+		},
+		{
+			in:   time.Hour*24*10e4 + time.Hour*12,
+			want: "100000.5 days ago",
+		},
 	}
-	for in, exp := range cases {
-		out := DescDuration(in)
-		if exp != out {
-			t.Fatalf("DescDuration(%v) => %q; want %q\n", in, out, exp)
+	for i, tt := range cases {
+		out := DescDuration(tt.in)
+		if tt.want != out {
+			t.Errorf("[%d] DescDuration(%v) => %q; want %q\n", i, tt.in, out, tt.want)
 		}
 	}
 }
